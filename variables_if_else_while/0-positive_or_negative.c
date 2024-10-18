557c62b98a891bda7c8e7ec6ff6b82f5f016992b
pablox85 <pdpcorrales@gmail.com>
2024-10-18 20:34:30 +0000

    C push

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -9,14 +9,9 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-if (n < 0) {
-  printf("Positive\n");
-} else if(n>0){
-  printf("Negative\n");
-} else {
-  printf("Zero");
+	if (n>0) 
+{
+	printf("Positive");
 }
 	return (0);
 }
-
-
