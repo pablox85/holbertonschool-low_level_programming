778af75e8d422064e485703b7ba8ea83f7365f6b
pablox85 <pdpcorrales@gmail.com>
2024-10-18 18:10:19 +0000

    C push

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,6 +8,10 @@
 int main(void)
 {
 	printf("Size of: %zu \n", sizeof(int));
+	printf("Size of: %zu \n", sizeof(char));
+	printf("Size of: %zu \n", sizeof(long int));
+	printf("Size of: %zu \n", sizeof(float));
+	printf("Size of: %zu \n", sizeof(long long int));
 	return (0);
 }
 
