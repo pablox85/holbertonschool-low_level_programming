5fafad30e2965cfe2bc2cb8ce11830f713ce9da6
pablox85 <pdpcorrales@gmail.com>
2024-10-18 18:01:52 +0000

    C push

---
+++ hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - entry ponint
+*
+*imprimir del tamano de varios tipos de archivos
+*Return:0
+*/
+int main(void)
+{
+	printf("Size of: %zu \n", sizeof(int));
+	return (0);
+}
+
+
