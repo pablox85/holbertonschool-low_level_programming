ae5459573f20c5b8c1cc3baed1980db8c411aaf7
pablox85 <pdpcorrales@gmail.com>
2024-10-21 17:31:55 +0000

    C push

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - 2-print_alphabet.c
+ *Return: 0
+ */
+
+	int main(void)
+	{
+	for (int i = 'a'; i <= 'z'; i++)
+	putchar (i);
+	return (0);
+	}
---
+++ variables_if_else_while/2-print_alphabet.cy
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*imprimir el abcdario
+*Return:0
+*/
+
+	int main(void)
+	{
+		char letra ='a'
+	letra++;
+	putchar(letra++);
+	
+	return(0);
+	}
