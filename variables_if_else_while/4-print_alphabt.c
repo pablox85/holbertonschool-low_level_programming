b9144cbcfc9ec93cda8f6b893baba8a18e904332
pablox85 <pdpcorrales@gmail.com>
2024-10-21 22:19:26 -0300

    pasar por betty

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+// corte en e
+//corte en q
+int main(void)
+{
+        char i;
+        for (i = 'a'; i <= 'z'; i++) 
+        if (i != 'e' && i != 'q')
+        {
+            putchar(i);
+        }
+{
+
+
+        putchar('\n');
+        return (0);
+}
+}
