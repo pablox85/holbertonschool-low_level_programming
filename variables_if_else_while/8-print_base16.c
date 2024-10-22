9de7540ea2bd39a26a80b9a2509178302540c9cb
pablox85 <pdpcorrales@gmail.com>
2024-10-22 00:28:06 -0300

    Push from Dell Latitud

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+
+    for (i = 0; i < 16; i++) 
+    {
+        if (i < 10) 
+        {
+            putchar(i + '0');  
+        } 
+        else
+        {
+            putchar(i - 10 + 'a'); 
+        }
+    }
+
+    putchar('\n');  
+    return (0);
+}
+
