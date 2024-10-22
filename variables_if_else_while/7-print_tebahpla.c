a1c294abcb6fa15880f1be25d359a06380729f66
pablox85 <pdpcorrales@gmail.com>
2024-10-22 14:44:43 +0000

    C

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print alphabet
+ * Return: 0
+ */
 
 int main(void)
 {
-        int i;
+	int i;
 
-        for (i = 'z'; i >= 'a'; i--)
-	putchar(i);
-	putchar('\n');
+	for (i = 'z'; i >= 'a'; i--)
+		putchar(i);
+		putchar('\n');
 	return (0);
 }
