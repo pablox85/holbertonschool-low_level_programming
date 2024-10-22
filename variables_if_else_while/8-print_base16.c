8d7818434e5d06fe4143d0566f05b62ec18d351e
pablox85 <pdpcorrales@gmail.com>
2024-10-22 13:37:21 +0000

    C

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * Description: del 0 al 9 y despues letras correspondientes
+ * hasta el 16
+ * Return: 0
+ */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < 16; i++) 
-    {
-        if (i < 10) 
-        {
-            putchar(i + '0');  
-        } 
-        else
-        {
-            putchar(i - 10 + 'a'); 
-        }
-    }
-
-    putchar('\n');  
-    return (0);
+	for (i = 0; i < 16; i++)
+	{
+		if (i < 10)
+		{
+			putchar(i + '0');
+		}
+		else
+		{
+			putchar(i - 10 + 'a');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
-
