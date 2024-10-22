01eaf8edf582673f4cd822a1b8e12992457168bc
pablox85 <pdpcorrales@gmail.com>
2024-10-22 13:49:09 +0000

    C

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
+/**
+ * main- entre pint
+ * description: escribe alfabeto sin la e y q
+ * Return: 0
+ */
 
 int main(void)
 {
-        char i;
-        for (i = 'a'; i <= 'z'; i++) 
-        if (i != 'e' && i != 'q')
-        {
-            putchar(i);
-        }
-{
-
-
-        putchar('\n');
-        return (0);
-}
+	char i;
+		for (i = 'a'; i <= 'z'; i++)
+			if (i != 'e' && i != 'q')
+			}
+			putchar(i);
+			}
+	putchar('\n');
+	return (0);
 }
