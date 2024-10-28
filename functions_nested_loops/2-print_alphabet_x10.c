ff59affde2db1e3816cca83da35d32945b5c3013
pablox85 <pdpcorrales@gmail.com>
2024-10-28 17:31:26 +0000

    Push from Dell Latitud

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,15 @@
  */
 void print_alphabet_x10(void)
 {
-        int i;
-        int j;
-        
-        for (i = 0; i <= 9; i++)
-        {
-                for (j = 'a'; j <= 'z'; j++)
-                {
-                        _putchar(j);
-                }
-                _putchar('\n');
-        }
+	int i;
+	int j;
+
+		for (i = 0; i <= 9; i++)
+		{
+          for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+	}
 }
