4fe263f979e0a6ddc2fc1125c1b8163ccaf03952
pablox85 <pdpcorrales@gmail.com>
2024-10-31 14:29:57 +0000

    Push from Dell Latitud

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,20 +1,14 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * asjdlasdjas
+ * Return:0
  */
-int main(void)
-{
-    int r;
 
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+int _islower(int c)
+{
+	if (c >= 'a'&& c <= 'z')
+		return(1);
+	else
+		return(0);
 }
--- functions_nested_loops/3-main.c
+++ functions_nested_loops/3-main.c
@@ -9,13 +9,11 @@ int main(void)
 {
     int r;
 
-    r = _isalpha('H');
+    r = _islower('H');
     _putchar(r + '0');
-    r = _isalpha('o');
+    r = _islower('o');
     _putchar(r + '0');
-    r = _isalpha(108);
-    _putchar(r + '0');
-    r = _isalpha(';');
+    r = _islower(108);
     _putchar(r + '0');
     _putchar('\n');
     return (0);
