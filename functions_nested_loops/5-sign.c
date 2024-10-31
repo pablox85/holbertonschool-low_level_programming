2367ad879339e84d567de4ee9cd528cc8ca7451c
pablox85 <pdpcorrales@gmail.com>
2024-10-31 15:34:41 +0000

    Push from Dell Latitud

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_sign - Determina el signo de un número
+ * @n: El número que se evaluará
+ * Return: 1 si el número es positivo, 0 si es cero, -1 si es negativo
+ */
+
+
+int print_sign(int n)
+
+{
+
+	if (n > 0)
+		return (+1);
+	else if (0 == 0)
+		return (0);
+	else
+		return (-1);
+
+}
