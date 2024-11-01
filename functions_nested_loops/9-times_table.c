a48c0911c440b9961c848aff15bd25a73dfcc14b
pablox85 <pdpcorrales@gmail.com>
2024-11-01 13:28:14 +0000

    Push from Dell Latitud

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,31 +1,32 @@
 #include "main.h"
 
 /**
- * times_table - Imprime la tabla de multiplicar del 0 al 9
+ * times_table - tabla del 9
+ * Return:0
  */
+
 void times_table(void)
+
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
 {
-    int i, j, product;
+	for (j = 0; j <= 9; j++)
+	{
+		k = i * j;
+	
+		if (j > 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
 
-    // Bucle externo para cada fila de la tabla (0 a 9)
-    for (i = 0; i <= 9; i++) {
-        // Bucle interno para cada columna de la tabla (0 a 9)
-        for (j = 0; j <= 9; j++) {
-            product = i * j; // Calcula el producto
-
-            // Imprimir el producto
-            if (j > 0) { // A partir de la segunda columna, imprime coma y espacio
-                _putchar(','); // Imprime la coma
-                _putchar(' '); // Imprime el espacio
-            }
-
-            // Imprimir el número (puede tener uno o dos dígitos)
-            if (product >= 10) {
-                _putchar((product / 10) + '0'); // Imprime la decena
-            }
-            _putchar((product % 10) + '0'); // Imprime la unidad
-        }
-        _putchar('\n'); // Salto de línea al final de cada fila
-    }
+		if (j >= 10)
+		{
+			_putchar((k / 10) + '0');
+		}
+			_putchar((k % 10) + '0');
+		}
+	_putcahr('\n');
+	}
 }
-
