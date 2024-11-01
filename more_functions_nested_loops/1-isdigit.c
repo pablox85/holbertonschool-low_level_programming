7449b821917984ac08517190163525aedd1c167a
pablox85 <pdpcorrales@gmail.com>
2024-11-01 16:09:10 +0000

    Push from Holberton

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- *_isdigit - djsaldsla
- *@c: daslÃ±dkasÃ
- *Return:sdjlaskdjlas
+ *_isdigit - Checks if a character is a digit
+ *@c: The character to check
+ *
+ *Return: Return: 1 if c is a digit (0-9), 0 otherwise
  */
 
 int _isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
 		return (1);
-	else 
+	else
 		return (0);
 }
 
