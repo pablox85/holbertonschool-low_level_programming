2c4907392b54b5e473892412271bd1ef98ddd64d
pablox85 <pdpcorrales@gmail.com>
2024-11-01 15:48:46 +0000

    Push from Holberton

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_isdigit - djsaldsla
+ *@c: daslÃ±dkasÃ
+ *Return:sdjlaskdjlas
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
+
