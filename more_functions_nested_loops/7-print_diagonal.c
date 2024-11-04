31d3edc121591228c37abbed7cbfefed8303cf12
pablox85 <pdpcorrales@gmail.com>
2024-11-04 14:23:58 +0000

    Push from Holberton

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - sadasd
+ * @s: number of slashes
+ *
+ * Return: slashes
+ */
+
+void print_diagonal(int s)
+{
+	int a, b;
+
+	if (s > 0)
+	{
+		for (a = 0; a < a; a++)
+		{
+			for (b = 0; b < row; b++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
