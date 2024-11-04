dbe10d71c2dd882dfa0b00a3b34868d859144a60
pablox85 <pdpcorrales@gmail.com>
2024-11-04 03:09:56 -0300

    Push from Holberton

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *
+ *
+ */
+
+void more_numbers(void)
+
+{
+	char nums [] ="01234567891011121314\n";
+	int i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 20; i++)
+		{
+			_putchar(nums[i]);
+		}
+
+_putchar('\n');
+
+
+
+
+
+
+	}
+}
+
