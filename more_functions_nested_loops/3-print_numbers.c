312b4d2da1f017104b846c76a61a1cc16cf81fc7
pablox85 <pdpcorrales@gmail.com>
2024-11-04 02:00:31 -0300

    Push from Holberton

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,24 @@
 #include "main.h"
+
 /**
-*print_numbers - imprime numeros
-*Return:0
-*
-*/
-
-void print_numbers(void)
-
+ * print_most_numbers - Imprime los números del 0 al 9, omitiendo el 2 y el 4,
+ * seguido de una nueva línea.
+ */
+void print_most_numbers(void)
 {
-int i;
-for (i = 0; i <= 9; i++)
-{
-	_putchar(i + '0');
+    int i;
 
-}
-	_putchar('\n');
+    for (i = 0; i <= 9; i++)
+    {
+        if (i == 2 || i == 4)
+        {
+            continue; // Salta la iteración si i es 2 o 4
+        }
+        else
+        {
+            _putchar(i + '0'); // Imprime el carácter numérico correspondiente
+        }
+    }
+    
+    _putchar('\n'); // Imprime una nueva línea al final
 }
--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -4,11 +4,29 @@
 *
 */
 
-void print_most_numbers(void);
+void print_most_numbers(void)
 
 {
 	
 int i;
-for (i = 0; i <= 9; i++)
 
+for (i = 0; i <= 9; i++)
+{
+	if ( i == 2 || i == 4)
+       	{
+	continue;
+	}
+
+
+
+}
+	_putchar (i++);
+{
+	_putchar('\n');
+
+
+
+
+
+}
 }
