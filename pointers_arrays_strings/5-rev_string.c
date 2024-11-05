73dfb061a3a7b5c727af68c179f598c6e6f115fe
pablox85 <pdpcorrales@gmail.com>
2024-11-05 18:31:38 -0300

    Push from Holberton

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,18 +7,17 @@
  */
 void rev_string(char *s)
 {
-    int i, j;
-    char temp;
+	int i, j;
+	char temp;
 
-    for (i = 0; s[i] != '\0'; i++)
-        ;
+	for (i = 0; s[i] != '\0'; i++);
 
-    i--; 
+	i--; 
 
-    for (j = 0; j < i; j++, i--)
-    {
-        temp = s[j];
-        s[j] = s[i];
-        s[i] = temp;
-    }
+	for (j = 0; j < i; j++, i--)
+	{
+	temp = s[j];
+	s[j] = s[i];
+	s[i] = temp;
+	}
 }
