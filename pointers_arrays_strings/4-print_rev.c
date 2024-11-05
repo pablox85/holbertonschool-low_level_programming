e62603898fcecabff99836eb631514afdbc2c2c7
pablox85 <pdpcorrales@gmail.com>
2024-11-05 17:30:54 +0000

    Push from Holberton

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_rev - dar vuelta horacion
+* @s: puntero
+*
+*/
+
+
+void print_rev(char *s)
+{
+
+	int b = 0;
+
+	while (s[b] != '\0')
+	{
+		_putchar(s[b]);
+		b--;
+	}
+
+	_putchar('\n');
+}
