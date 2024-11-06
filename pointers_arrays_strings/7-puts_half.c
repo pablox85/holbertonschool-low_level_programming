fbb34b9be883a0991a114531eea32c5c306d610a
pablox85 <pdpcorrales@gmail.com>
2024-11-06 17:41:52 +0000

    Push from Holberton

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -9,14 +9,14 @@
 void puts_half(char *str)
 {
 
-	int i = 0;
+	int i = *str / 2;
+
 	for (; str[i] != '\0'; i++)
 	{
-		_putchar(i + '0');
+		_putchar(i+ '0');
+
 	}
-_putchar ('\n');
-
-
-
+	_putchar('\n');	
+ 
 
 }
