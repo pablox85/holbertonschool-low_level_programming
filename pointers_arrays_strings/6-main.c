df33eb1d47001c1bf086a5357c8a115502eae511
pablox85 <pdpcorrales@gmail.com>
2024-11-06 14:43:35 +0000

    Push from Holberton

--- pointers_arrays_strings/6-main.c
+++ pointers_arrays_strings/6-main.c
@@ -11,19 +11,4 @@ int main(void)
 
     str = "0123456789";
     puts2(str);
-#include "main.h"
- 
- /**
-  *  * main - check the code
-  *   *
-  *    * Return: Always 0.
-  *     */
- int main(void)
- {
-    char *str;
-
-    str = "0123456789";
-    puts2(str);
-    return (0);
- }    return (0);
 }
