5cd40cc5d21f7b09a50298772b24b7d66fea184a
pablox85 <pdpcorrales@gmail.com>
2024-11-07 16:54:44 +0000

    Push from Holberton

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -2,24 +2,35 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
- *
- *
- */
+*
+*
+*@n: tamaÃo de array
+*
+*
+*/
 void print_array(int *a, int n)
 {
 
 	int i = 0;
 
-	for ( i = 0; i < n; i++);
+	for (i = 0; i < n; i++)
+	{
 	printf("%d", a[i]);
+	}
 
-	if (i != n -1) 
+	if (i < n -1)
 	{
 		printf(", ");
 	}
 
+
+
+
+
+
+
+	
 	printf("\n");
+	
+
 }
