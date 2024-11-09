fd595a1cf81a99bbdd0c750b4ccc6ff5450d342b
pablox85 <pdpcorrales@gmail.com>
2024-11-09 18:37:58 +0000

    Push from Holberton

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -11,7 +11,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
         unsigned int i = 0;
 
 
-        while (i < n)
+        while (src[i] != '\0' && i < n)
         {
                 *dest++ = *src++;
                         i++;
