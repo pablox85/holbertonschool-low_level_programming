aa9f3981a09f7eb8d8eb39bce4e1f64cf01e6deb
pablox85 <pdpcorrales@gmail.com>
2024-11-09 20:37:31 +0000

    Push from Holberton

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,26 +11,16 @@
 void print_array(int *a, int n)
 {
 
-	int i = 0;
+int i = 0;
 
 	for (i = 0; i < n; i++)
 	{
-	printf("%d", a[i]);
+		printf("%d", a[i]);
+		if (i < n -1)
+		{
+			printf(", ");
+		}
 	}
-
-	if (i < n -1)
-	{
-		printf(", ");
-	}
-
-
-
-
-
-
-
-	
 	printf("\n");
-	
 
 }
