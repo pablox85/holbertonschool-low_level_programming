1d3f653ef3a695089bbe76d8ad0cdf085e017208
pablox85 <pdpcorrales@gmail.com>
2024-11-12 17:03:26 +0000

    Push from Holberton

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -11,4 +11,8 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
+char *_strcat(char *dest, char *src);
+
+
+
 char *_memset(char *s, char b, unsigned int n);
