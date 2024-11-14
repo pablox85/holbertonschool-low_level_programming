5f2c75763b99bb3444a738c6db1ddfcabd1653db
pablox85 <pdpcorrales@gmail.com>
2024-11-14 18:05:55 +0000

    Push from Holberton

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ *_strncpy - una locura
+ *@dest: destino
+ *@src: source
+ *@n: n es n
+ * @n: n es n
+ *Return: 0
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+
+int i = 0;
+
+while (i < n && src[i] != '\0')
+{
+	dest[i] = src[i];
+	i++
+}
+
+{
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
