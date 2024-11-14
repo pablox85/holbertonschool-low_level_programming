593dea07c7bfce1134e9c6b9ab5d69093cd1cad7
pablox85 <pdpcorrales@gmail.com>
2024-11-14 16:28:22 +0000

    Push from Holberton

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -5,28 +5,26 @@
  * @dest: destino
  * Return: dest
  * @n: n es n
+ *
  */
-
 char *_strncat(char *dest, char *src, int n)
-
 {
 	int i = 0;
 	int j = 0;
 
-while (dest[i] != '\0')
+	while (dest[i] != '\0'
+			)
 	{
-		 i++;
+		i++;
 	}
-
 	while (j < n && src[j] != '\0')
+
 	{
-		dest[i] = src[j];
+	dest[i] = src[j];
 		i++;
 		j++;
 	}
 
 	dest[i] = '\0';
-
 	return (dest);
-
 }
