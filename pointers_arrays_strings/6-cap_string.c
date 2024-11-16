4d5685c053a1223a2be58d811a0db4e1eae8518f
pablox85 <pdpcorrales@gmail.com>
2024-11-16 21:43:29 +0000

    Push from Holberton

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,1 +1,23 @@
 #include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >=97 && str[i] <= 122) && (str[i] = 9, 10, 32, 34 )
+		{
+			str[i] = str[i] - 32;
+			}
+		i++;
+			}
+
+return (str);
+	}
