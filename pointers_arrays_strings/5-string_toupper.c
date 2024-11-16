2da005e5dc965f4fdf5aa7eb39b17180ea179c88
pablox85 <pdpcorrales@gmail.com>
2024-11-16 20:57:40 +0000

    Push from Holberton

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,32 +1,22 @@
 #include "main.h"
 /**
- *
- *
- *
- *
- *
+ *string_toupper - function
+ *Return: str
+ *@char *: string
  */
 
 char *string_toupper(char *)
 {
 
+	int i = 0;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+		return (str);
 }
