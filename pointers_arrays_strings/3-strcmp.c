9963419dde8ab11968e3d3e986098505848f9275
pablox85 <pdpcorrales@gmail.com>
2024-11-16 05:08:26 +0000

    Push from Holberton

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,21 +1,23 @@
 #include "main.h"
 /**
- *
- *
- *
+ *_strcmp - copia
+ *@s1: string 1
+ *@s2: string 2
+ *Return: resultado
  */
-
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while (s1[i] != '\0' && s2[i] != '\0') {
-	if (s1[i] != s2[i]) {
-	return s1[i] - s2[i];
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+	{
+		return s1[i] - s2[i];
 	}
-	i++;
+		i++;
 	}
 
 
-return s1[i] - s2[i];
+return ( s1[i] - s2[i] );
 }
