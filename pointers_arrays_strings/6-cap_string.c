aa84c40cb886da2c4e8df2e56f7ba181d3c4b722
pablox85 <pdpcorrales@gmail.com>
2024-11-17 21:41:59 +0000

    Push from Holberton

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,14 +10,29 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (str[i] != '\0') /*busqueda carcter nulo*/
 	{
-		if (str[i] >=97 && str[i] <= 122) && (str[i] = 9, 10, 32, 34 )
-		{
-			str[i] = str[i] - 32;
-			}
-		i++;
-			}
-
-return (str);
+	if (str[i] >= 97 && str[i] <= 122
+		i == 0 ||
+		str[i -  1] == ',' ||
+		str [i - 1] == ';' ||
+		str [i - 1] == '.' ||
+		str [i - 1] == '!' ||
+		str [i - 1] == '?' ||
+		str [i - 1] == '"' ||
+		str [i - 1] == '(' ||
+		str [i - 1] == ')' ||
+		str [i - 1] == '{' ||
+		str [i  -1] == '}' ||
+		str [i - 1] == ' ' ||
+		str [i - 1] == '\t'||
+		str [i - 1] == '\n'||
+		)
+	{
+	
 	}
+
+	}
+
+
+	/* str[i] = str[i] - 32; mayusculas */
