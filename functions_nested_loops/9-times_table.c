04bc3067fc0e2bfa49b53eeb0d0e6cc6a907bd8e
pablox85 <pdpcorrales@gmail.com>
2024-11-28 12:43:20 +0000

    Push from Dell Latitud

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,26 +7,27 @@
 
 void times_table(void)
 {
-	int num1;
-	int num2;
-	int num3;
+        int num1;
+        int num2;
+        int num3;
 
-	for (num1 = 0; num1 <= 9; num1++)
+        for (num1 = 0; num1 <= 9; num1++)
 
-	for (num2 = 0; num2 <= 9; num2++)
-	{
-		num3 = num1 * num2;
-		if (num2 > 0)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
+        for (num2 = 0; num2 <= 9; num2++)
+        {
+                num3 = num1 * num2;
+                if (num2 > 0)
+                {
+                        _putchar(',');
+                        _putchar(' ');
+                }
 
-		if (num2 >= 10)
-		{
-			_putchar((num3 / 10) + '0');
-		}
-			_putchar((num3 % 10) + '0');
-		}
-	_putchar('\n');
-	}
+                if (num2 >= 10)
+                {
+                        _putchar((num3 / 10) + '0');
+                }
+                        _putchar((num3 % 10) + '0');
+                }
+        _putchar('\n');
+        }
+~
