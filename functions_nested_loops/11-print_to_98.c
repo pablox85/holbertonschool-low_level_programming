570fda07e55bda2983394659cf37943ecee2e508
pablox85 <pdpcorrales@gmail.com>
2024-12-03 16:06:53 +0000

    Push from Dell Latitud

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+void print_to_98(int n)
+	#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_to_98- despues de pensar 3 horas con el 9 este no puede ser tan facil
+ *@n: los numeros
+ *Return: n + a
+ */
+
+void print_to_98(int n)
+{
+int i;
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+				if (i < 98)
+				{
+					printf(", ");
+				}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+				if (i > 98)
+				{
+					printf(", ");
+				}
+		}
+	}
+	printf("\n");
+}
