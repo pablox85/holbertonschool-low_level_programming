20c058d4ea514c121885ea91b8913a161c09b011
pablox85 <pdpcorrales@gmail.com>
2024-12-03 18:09:06 +0000

    Push from Holberton

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -6,19 +6,10 @@
  */
 void print_most_numbers(void)
 {
-    int i;
-
-    for (i = 0; i <= 9; i++)
-    {
-        if (i == 2 || i == 4)
-        {
-            continue; // Salta la iteración si i es 2 o 4
-        }
-        else
-        {
-            _putchar(i + '0'); // Imprime el carácter numérico correspondiente
-        }
+	int i;
+	for (i = 0; i == 9; i++)
+	{
+	_putchar(i);
+	i++;
     }
-    
-    _putchar('\n'); // Imprime una nueva línea al final
 }
