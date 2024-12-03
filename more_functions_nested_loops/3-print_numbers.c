e60a04173f3ad7cc62be1603146dea3f7be38d9e
pablox85 <pdpcorrales@gmail.com>
2024-12-03 18:21:35 +0000

    Push from Holberton

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Imprime los números del 0 al 9, omitiendo el 2 y el 4,
+ * print_numbers - Imprime los números del 0 al 9, omitiendo el 2 y el 4,
  * seguido de una nueva línea.
  */
 void print_numbers(void)
 {
 	int i;
+
 	for (i = 0; i <= 9; i++)
 	{
-	_putchar(i);
-	i++;
-    }
+	_putchar (i + '0');
+	}
+	_putchar ('\n');
 }
