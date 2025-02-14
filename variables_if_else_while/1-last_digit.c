a2b74d4347bcb4d07c7eeed04f69cf5ff44c8a9a
root <pdpcorrales@gmail.com>
2025-02-14 20:11:10 +0000

    elif varibles

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -9,33 +9,34 @@
 
 int main(void)
 {
-	int n;
-	int dig = 0;
+        int n;
+        int dig = 0;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-        
-	dig = n % 10;
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
 
-	if (dig > 5) 
-	{
-		printf("The last digit of %d",n);
-		printf(" is %d", dig );
- 		printf(" and is less than 6 and not 0");
-	}
-	else if (dig < 5)
-	{
-		printf("The last digit of %d",n);
-		printf(" is %d", dig );
-		printf(" and is greater than 5");
-       } else 
+        dig = n % 10;
+
+        if (dig >= 7)
+        {
+                printf("The last digit of %d",n);
+                printf(" is %d", dig );
+                printf(" and is greater than 5\n");
+ 
+        }
+        else if (dig <= 5 && dig != 0)
+        {
+                printf("The last digit of %d",n);
+                printf(" is %d", dig );
+                printf(" and is less than 6 and not 0\n");
+       } else
        {
-		printf("The last digit of %d",n);
- 		printf(" is %d", dig );
- 		printf(" and is 0");
+                printf("The last digit of %d",n);
+                printf(" is %d", dig );
+                printf(" and is 0 \n");
        }
-       
+
         /* your code goes there */
-        
+
         return (0);
 }
