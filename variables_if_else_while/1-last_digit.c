dbfbbc6e8d1d4954640e5ad7f186faa75bc78323
root <pdpcorrales@gmail.com>
2025-02-15 17:32:01 +0000

    elif varibles

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,19 +19,19 @@ int main(void)
 
 	if (dig >= 7)
 	{
-		printf("The last digit of %d", n);
+		printf("Last digit of %d", n);
 		printf(" is %d", dig);
 		printf(" and is greater than 5\n");
 	}
 	else if (dig == 0)
 	{
-	printf("The last digit of %d", n);
+	printf("Last digit of %d", n);
 	printf(" is %d", dig);
 	printf(" and is 0 \n");
 	}
 	else
 	{
-		printf("The last digit of %d", n);
+		printf("Last digit of %d", n);
 		printf(" is %d", dig);
 		printf(" and is less than 6 and not 0\n");
 	}
