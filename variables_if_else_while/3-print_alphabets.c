b6c8a58f471b0a0d692f84ad58b3b8eeca22df6f
root <pdpcorrales@gmail.com>
2025-02-16 21:57:34 +0000

    elif varibles

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - sarasa
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+	int h;
+
+	for (i = 97; i <= 122; i++)
+
+	{
+		putchar(i);
+	}
+	for (h = 65; h <= 90; h++)
+	{
+		putchar(h);
+	}
+
+putchar('\n');
+
+	return (0);
+}
