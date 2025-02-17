a0d6685e2bdf5aaa288f07fa846a795cb2f0f38b
root <pdpcorrales@gmail.com>
2025-02-17 15:41:15 +0000

    fast push

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print 10x el abcdario
+ */
+
+void print_alphabet_x10(void)
+{
+	
+	char i; /* outer */
+	int j;
+
+		for (j = 0; j <= 9; j++) 
+		{
+			for (i = 'a'; i <= 'z'; i++)
+				_putchar(i);
+			_putchar('\n');
+		}
+
+		}
