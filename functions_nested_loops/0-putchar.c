e92e5cbe4765265b6973e07920bc358e3f935862
root <pdpcorrales@gmail.com>
2025-02-17 01:23:15 +0000

    fast push

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
 #include "main.h"
 /**
- * main - call to _putchar
+ * main- EXPLIQUEN MEJOR LAS LETRAS
  * Return: 0
  */
-
 int main(void)
 {
-	printf("_putchar");
-}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 return (0);
+
 }
