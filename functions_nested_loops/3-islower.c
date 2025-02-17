1c71e4145f236d00fb3404037fc9634dccb67760
root <pdpcorrales@gmail.com>
2025-02-17 17:27:27 +0000

    fast push

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,13 +1,18 @@
 #include "main.h"
+/**
+ * _islower - check lower letters
+ * @c: - sartasa
+ * Return: 0
+ */
 
 int _islower(int c)
 {
 
-	if (c >= 97 && c <= 122) 
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
-	else 
+	else
 	{
 		return (0);
 	}
