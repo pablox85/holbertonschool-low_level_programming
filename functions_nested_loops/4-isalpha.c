c1b55deacc3d850eebdebe76386649b021f49c6e
root <pdpcorrales@gmail.com>
2025-02-17 17:47:31 +0000

    fast push

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,25 +1,25 @@
 #include "main.h"
 /**
- * _isalpha - sasrtasa
- * @c: mas sarasa
- * Return;: 0
+ *_isalpha - sasrtasa
+ *@c: mas sarasa
+ *Return: 0
  */
 
 int _isalpha(int c)
 
 {
 
-	if (c >= 97 && c <= 122 )
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
 	else if (c >= 65 && c <= 90)
 	{
 		return (1);
- 	}
+	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 
 }
