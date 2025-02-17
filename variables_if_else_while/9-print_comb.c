9ea6ca51ef53492cb7e9140142b97a0e6dc0ed3e
root <pdpcorrales@gmail.com>
2025-02-17 00:27:15 +0000

    elif varibles

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -sarasa
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int h = ',';
+	int j = ' ';
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		putchar(h);
+		putchar(j);
+	}
+putchar('\n');
+return (0);
+}
