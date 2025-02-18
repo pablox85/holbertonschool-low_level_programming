6d9fd54138cdddfa4cd0336c950abb4792b7a2ff
root <pdpcorrales@gmail.com>
2025-02-18 16:58:17 +0000

    fast push

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,14 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  *print_last_digit - sasas
  *Return: 0
- @r: asa
+ *@r: - asas
+ *
  */
 
 int print_last_digit(int r)
 {
-	int digito;
 
-	digito = r % 10;
-	return (digito);
+	return (r);
 }
