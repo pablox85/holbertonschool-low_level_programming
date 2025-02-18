342cf28602af17c80d71af1bc301c9750e18ff19
root <pdpcorrales@gmail.com>
2025-02-18 16:04:06 +0000

    fast push

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs  - funcion saraseadora
+ * Return: 0
+ * @r: sarasa
+ */
+
+int _abs(int r)
+{
+	if (r < 0)
+	{
+		return (-r);
+	}
+	else
+	{
+	return (r);
+	}
+}
