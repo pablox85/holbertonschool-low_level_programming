c36b065caa5a740221b8654e3e3e4305a96ffff8
root <pdpcorrales@gmail.com>
2025-02-18 19:36:33 +0000

    fast push

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,25 +8,32 @@
 
 void jack_bauer(void)
 {
-int i;
+int i; /* HORAS */
+int j; /* MINUTOS 0 al 5 */
+int k; /* MINUTOS 0 al 9 */
 
-	for (i = 48; i <= 50; i++)
+	for (i = 48; i <= 57; i++) /*0 al 2*/
+		for (j = 48; j <= 53; j++) /*0 al 5*/
+			for(k = 48; k <= 57; k++)
 	{
 	if (i == 48)
 	{
 		_putchar(i);
 		_putchar(i);
-		_putchar(32);
-
+		_putchar(58);
+		_putchar (j);
+		_putchar(k);
 	}
-	else if (i > 0)
+	
+	if (i > 48)
 	{
 	_putchar(48);
 	_putchar(i);
 	_putchar(58);
-	_putchar(65);
+	_putchar(j);
+	_putchar(k);
 	}
 	_putchar(10);
-
 	}
+
 }
