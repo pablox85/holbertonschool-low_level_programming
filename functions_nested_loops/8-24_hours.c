90399230b5f97d939de0dc4af566eb37ce5c1477
pablox85 <pdpcorrales@gmail.com>
2025-02-19 19:57:36 +0000

    fast push

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,30 +1,23 @@
 #include "main.h"
 /**
- *
- *
- *
+ * jack_bauer - contador de horas
  *
  */
 
 void jack_bauer(void)
 {
-int i; /* HORAS */
-int j; /* HORAS 0 al 3 */
-int k; /* MINUTOS 0 al 5 */
-int l; /* MINUTOS 0 AL 9 */
+	int i, j, k, l;
 
-	for (i = '0'; i <= '2'; i++) /*0 al 2*/
-		for (j = '0'; j <= '3'; j++) /*0 al 3*/
-			for(k = '0'; k <= '5'; k++)
+	for (i = '0'; i <= '2'; i++)
+		for (j = '0'; j <= '3'; j++)
+			for (k = '0'; k <= '5'; k++)
 				for (l = '0'; l <= '9'; l++)
-	{
-		_putchar(i);
-		_putchar(j);
-		_putchar(58);
-		_putchar (k);
-		_putchar(l);
-		_putchar(10);
-	}
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(58);
+					_putchar (k);
+					_putchar(l);
+					_putchar(10);
+				}
 }
-
-
