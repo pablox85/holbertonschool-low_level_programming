cfb9fa86bbc9eb2ffcf921e54d2f6e6dcba65e86
pablox85 <pdpcorrales@gmail.com>
2025-02-19 21:11:41 +0000

    fast push

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -6,18 +6,21 @@
 
 void jack_bauer(void)
 {
-	int i, j, k, l;
+	{	int i, j, k, l;
 
 	for (i = '0'; i <= '2'; i++)
-		for (j = '0'; j <= '3'; j++)
+		for (j = '0'; j <= '9'; j++)
+		if (i == '2' && j > '3')
+		break;
 			for (k = '0'; k <= '5'; k++)
 				for (l = '0'; l <= '9'; l++)
-				{
-					_putchar(i);
-					_putchar(j);
-					_putchar(58);
-					_putchar (k);
-					_putchar(l);
-					_putchar(10);
-				}
+				_putchar(i);
+				_putchar(j);
+				_putchar(58);
+				_putchar(k);
+				_putchar(l);
+				_putchar(10);
+	if (i == '2' && j == '3' && k == '5' && l == '9')
+	return;
+	}
 }
