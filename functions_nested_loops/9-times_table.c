a781d990a6a40d17ca82d37a35a846102cdfa79f
pablox85 <pdpcorrales@gmail.com>
2025-02-20 19:52:41 +0000

    fast push

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -2,57 +2,69 @@
 
 void times_table(void)
 {
-        int i, j;
+        int j;
+	
+		for (j = '0'; j <= '9'; j++)
+	{
+			_putchar(j), _putchar(','), _putchar(' ');
+	
+	}
+		_putchar('\n');
 
-
-        for (i = '0'; i <= '9'; i++)
-        {
-                for (j = '0'; j <= '9'; j++)
-                        /*filas y columnas 9X9*/
-                _putchar(j), _putchar(','), _putchar(' ');
-                _putchar('\n'); break;
-
-        }
-        {
+        
+	{
                  for (j = '0'; j <= '9'; j+=2)
                           _putchar(j), _putchar(','), _putchar(' ');
- 	             _putchar('\n');
         }
+		_putchar('\n');
+	
+	
 	{ 
 		for (j = '0'; j <= '9'; j+=3) 
 			_putchar(j), _putchar(','), _putchar(' ');
-		_putchar('\n');
 	}
+		_putchar('\n');
+	
+	
 	{
 		for (j = '0'; j <= '9'; j+=4)
 			_putchar(j), _putchar(','), _putchar(' ');
-		_putchar('\n');
 	}
-        {
+		_putchar('\n');
+		
+        
+	{
                 for (j = '0'; j <= '9'; j+=5)
                         _putchar(j), _putchar(','), _putchar(' ');
-                _putchar('\n');
         }
-        {
+		_putchar('\n');
+        
+	
+	{
                 for (j = '0'; j <= '9'; j+=6)
                         _putchar(j), _putchar(','), _putchar(' ');
-                _putchar('\n');
         }
-        {
+		_putchar('\n');
+        
+	
+	{
                 for (j = '0'; j <= '9'; j+=7)
                         _putchar(j), _putchar(','), _putchar(' ');
-                _putchar('\n');
 	}
-        {       
+		_putchar('\n');
+        
+	
+	{       
 		for (j = '0'; j <= '9'; j +=8)
 			_putchar(j), _putchar(','), _putchar(' ');
-		_putchar('\n');	
         }
-		for (j = '0'; j <= '9'; j+=9)
-		{
-			_putchar(j); _putchar(','); _putchar(' ');
+		_putchar('\n');
 		
-		}
+		for (j = '0'; j <= '9'; j+=9)
+	{
+			_putchar(j); _putchar(','); _putchar(' ');
+	
+	}
 		_putchar('\n');
  
 }
