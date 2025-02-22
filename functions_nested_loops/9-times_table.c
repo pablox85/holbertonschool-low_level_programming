5ace2f43c8a60f6b19ccf1996e1d15c2b8e349e3
pablox85 <pdpcorrales@gmail.com>
2025-02-22 01:21:10 +0000

    fast push

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,34 +1,30 @@
 #include "main.h"
 /**
  * times_table - tabla pitagorica
- *@i- filas
- *@j- columnas
  */
+
 void times_table(void)
 {
-	int i, j, result;
+	int num, mult, prod;
 
-		for (i = 0; i <= 9; i++)
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
 		{
-			for (j = 0; j <= 9; j++)
-			{
-			result = i * j;
-
-			if (j != 0)
-			{
 			_putchar(',');
 			_putchar(' ');
-			}
 
-			if (result < 10)
-			_putchar(result + '0');
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
 			else
-			{
-			_putchar(result / 10 + '0');
-			_putchar(result % 10 + '0');
-			}
-			}
-		_putchar('\n');
-		}
-}
+				_putchar((prod / 10) + '0');
 
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
