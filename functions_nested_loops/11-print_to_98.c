1906a1ee961e5c55c9b7394b7b8e9a0905c52e71
pablox85 <pdpcorrales@gmail.com>
2025-02-23 05:04:14 +0000

    fast push

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include "stdio.h"
 /**
- * n - valor asignado
- *
+ * print_to_98 - cuenta hasta98
+ * @n: numero
  */
 
 void print_to_98(int n)
@@ -13,31 +13,30 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 97; i++)
 		{
-			printf("%d, ",i);
+			printf("%d, ", i);
 			if (i == 97)
 			{
 			printf("98\n");
-			
-			}	
+			}
 		}
 	}
 	else if (n > 98)
 	{
-		 for (i = n; i >= 99; i--)
-		 {
+		for (i = n; i >= 99; i--)
+		{
 			printf("%d, ", i);
 			if (i == 99)
 			{
 			printf("98\n");
-			}	
+			}
 		}
-	}	 
-	else 
+	}
+	else
 	{
 		n = 98;
-		printf("%d\n",n);
+		printf("%d\n", n);
 	}
-	
+
 
 
 }
