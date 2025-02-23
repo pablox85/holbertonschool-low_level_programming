e7d36d1247190d98c5b615fe98199570cfa6c4b8
pablox85 <pdpcorrales@gmail.com>
2025-02-23 02:57:04 +0000

    fast push

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -7,24 +7,5 @@
 
 void print_to_98(int n)
 {
-	int i;
-	printf("elige un numero ");
-	scanf("%d", &n);
-
-	if (n < 0 )
-	{
-		for (i = n; i <= 98; i++)
-		{
-			printf("%d\n",i);
-			
-		}
-	 }
-	else if ( n > 98)
-	{
-		 for (i = n; i >= 98; i--)
-		 {
-			printf("%d\n",i);
-		}	
-	}
-
+	print_to_98(n);
 }
