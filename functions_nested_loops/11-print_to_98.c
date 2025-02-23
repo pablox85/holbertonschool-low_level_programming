75e1a0a8539d11fae65817a9df794db978a0fd43
pablox85 <pdpcorrales@gmail.com>
2025-02-23 03:55:51 +0000

    fast push

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -23,10 +23,10 @@ void print_to_98(int n)
 	 }
 	else if (n > 98)
 	{
-		 for (i = n; i >= 98; i--)
+		 for (i = n; i >= 99; i--)
 		 {
 			printf("%d, ", i);
-			if (i == 98)
+			if (i == 99)
 			{
 			printf("98");
 			}	
