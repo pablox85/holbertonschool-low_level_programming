5679458d834f9af2cafa7f1f1dbed3ca1c1303bf
pablox85 <pdpcorrales@gmail.com>
2025-02-23 02:52:00 +0000

    fast push

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,18 +11,20 @@ void print_to_98(int n)
 	printf("elige un numero ");
 	scanf("%d", &n);
 
-	if (n > 0 && n < 98)
+	if (n < 0 )
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d\n", i);
+			printf("%d\n",i);
+			
 		}
-	 } 
+	 }
 	else if ( n > 98)
 	{
 		 for (i = n; i >= 98; i--)
 		 {
-			printf("%d\n ", i);
+			printf("%d\n",i);
 		}	
-	} 
+	}
+
 }
