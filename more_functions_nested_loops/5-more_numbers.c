20a2b7be2d9109bfdeef9fc875664f295e54d313
pablox85 <pdpcorrales@gmail.com>
2025-02-24 17:35:55 +0000

    more loops

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,8 +8,10 @@
 void more_numbers(void)
 {
 
-	int j;
+	int i,j;
 	
+	for (i = 0; i <= 9; i++)
+	{
 		for ( j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
@@ -17,7 +19,7 @@ void more_numbers(void)
 				_putchar (j / 10 + '0');
 			}
 				_putchar (j % 10 + '0');
-		}
+	}
 		_putchar('\n');
-
+	}
 }
