df913c99854069bafb99be211f82f848a21862fa
pablox85 <pdpcorrales@gmail.com>
2025-02-24 18:30:58 +0000

    more loops

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+
+void print_line(int n)
+{
+
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n <= '0')
+		{
+			_putchar(10);
+		}
+		else 
+		{
+			_putchar(i);
+		}
+		_putchar(10);
+	}
+
+
+}
