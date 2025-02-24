f470e0114c5a462ed7288005ba81c804921960b0
pablox85 <pdpcorrales@gmail.com>
2025-02-24 19:07:56 +0000

    more loops

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -12,14 +12,14 @@ void print_line(int n)
 
 	for (i = 0; i <= n; i++)
 	{
-		if (n <= 0)
-		{
-			_putchar(10);
-		}
 		if (n > 0)
 		{
-			
 			_putchar(95);
+		}
+		if (n <= 0)
+		{
+			
+			_putchar(10);
 			
 		}
 	
