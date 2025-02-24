78fd3028b96de3b2ed0531f95f384d6695f3cc7f
pablox85 <pdpcorrales@gmail.com>
2025-02-24 16:26:20 +0000

    more loops

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_numbers - imprime del 0 a 9
+ *
+ *
+ *
+ */
+
+void print_numbers(void)
+{
+
+	int i;
+		for (i = 48; i <= 57; i++)
+			{
+			_putchar(i);
+			}
+		_putchar(10);
+}
