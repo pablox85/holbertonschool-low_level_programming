ca9672e4cdc4e282520d5b1decf499c521cd49da
pablox85 <pdpcorrales@gmail.com>
2025-02-24 17:46:46 +0000

    more loops

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *
+ *more_numbers - mas numeros
  *
  *
  */
@@ -8,18 +8,15 @@
 void more_numbers(void)
 {
 
-	int i,j;
+	int j;
 	
-	for (i = 0; i <= 9; i++)
-	{
 		for ( j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
 			{
-				_putchar (j / 10 + '0');
+				_putchar (j / 10 + 48);
 			}
-				_putchar (j % 10 + '0');
+				_putchar (j % 10 + 48);
 		}
 		_putchar('\n');
-	}
 }
