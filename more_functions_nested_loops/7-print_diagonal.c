86061ebd529385f1eda86c191a3cffe94eb1a144
pablox85 <pdpcorrales@gmail.com>
2025-02-25 16:45:30 +0000

    more loops

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -11,16 +11,15 @@ void print_diagonal(int n)
 
 	for (i = 1; i <= n; i++)
 	{
-		if (n > 0)
-		{
-			_putchar(32);
-			
-		}
-		else
+		if (n <= 0)
 		{
 			_putchar(10);
 		}
-
+		else if (n >= 1)
+		{
+			_putchar(32);
+		}
 	}
+	_putchar(92);
 	_putchar(10);
 }
