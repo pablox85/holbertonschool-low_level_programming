a17700c250a52407ef50483a9f47c5a1a00cb7f1
pablox85 <pdpcorrales@gmail.com>
2025-02-25 17:18:03 +0000

    more loops

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *
+ *
+ * 35 == #
+ *
+ * 
+ *
+ *
+ */
+
+void print_square(int size)
+{
+
+	int i;
+	if (size <= 0)
+		_putchar(10);
+
+	for (i = 1; i <= size; i++)
+	{
+		_putchar(35);
+	}
+_putchar(10);
+
+}
