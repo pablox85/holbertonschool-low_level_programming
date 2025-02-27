8d7ce5d2e6e3b17379c661affe50e501e48b1839
pablox85 <pdpcorrales@gmail.com>
2025-02-27 18:14:13 +0000

    C pointers

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ */
+
+int _strlen(char *s)
+
+{
+	int i = 0;
+	while (*s != '\0')
+	
+	{
+
+	i++;
+	
+
+	}
+	printf("%d", i);
+
+	return(0);
+}
