6d9d406e038ad94160835fdb4ac0688a7779fe66
pablox85 <pdpcorrales@gmail.com>
2025-02-27 19:03:35 +0000

    C pointers

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,24 +1,21 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
- *
- *
+ *_strlen - calcula los caractecres
+ *@s: - contador
+ *Return: devuelve el valor de la cadena
  */
 
 int _strlen(char *s)
 
 {
-	int i = 0;
-	while (*s != '\0')
-	
+
+	int i;
+
+		while (s[i] != '\0')
 	{
-
-	i++;
-	
-
+		i++;
 	}
-	printf("%d", i);
 
-	return(0);
+return (i);
 }
