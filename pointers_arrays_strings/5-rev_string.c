21cb29901fdc2a26f039a74e8d4d1b309abe1749
pablox85 <pdpcorrales@gmail.com>
2025-02-28 15:22:37 +0000

    C pointers

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *rev_string - rev string
+ *@s: - parametro dado
+ *
+ */
+
+void rev_string(char *s)
+{
+
+int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	i--;
+
+	while (i >= 0)
+	{
+		i--;
+	}
+}
+
