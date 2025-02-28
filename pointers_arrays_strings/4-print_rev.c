30fe42fe927fdbc35502e00da74bb1673d38b0e8
pablox85 <pdpcorrales@gmail.com>
2025-02-28 13:58:15 +0000

    C pointers

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+
+void print_rev(char *s)
+{
+
+
+	int i = 0;
+
+	while (s[i] != '\0')
+		{
+			i++;
+		}
+
+		while(s[i] != '\0')
+		{
+			_putchar(s[i]);
+			i--;
+		}
+
+}
