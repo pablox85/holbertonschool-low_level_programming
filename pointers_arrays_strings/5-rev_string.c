27b5c2146cefd420bbc6d68ab5c0d414b05e48e8
pablox85 <pdpcorrales@gmail.com>
2025-02-28 18:02:32 +0000

    C pointers

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -9,17 +9,23 @@ void rev_string(char *s)
 {
 
 int i = 0;
-int j = 500;
-int temp;
+int j;
+int largo = 0;
+
+
+	while (s[i] != '\0')
+	{
+		largo++;
+	}
+		j = largo -1;
 
 	while (i < j)
 	{
-		temp = s[i];
+		largo = s[i];
 		s[i] = s[j];
-		s[j] = temp;
-		i++;
-		j--;
+		s[j] = largo;
 	}
-return 0;
+	i++;
+	j--;
 }
 
