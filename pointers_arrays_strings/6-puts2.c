4535a0b32a500965485879903134abdc6730ca1f
pablox85 <pdpcorrales@gmail.com>
2025-03-01 20:13:11 +0000

    C pointers

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts2: - asrasa
+ *
+ * imprimir caracter de 2 en 2
+ * 012345678
+ * 02468...
+ *
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+		_putchar(str[(i -1) 2]);
+	}
+_putchar(10);
+
+
+
+}
