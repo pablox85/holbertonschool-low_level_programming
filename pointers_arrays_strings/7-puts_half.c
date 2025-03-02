3e18c4d38eb4fe9525c9b6ac8b0282f0a7df2c0a
pablox85 <pdpcorrales@gmail.com>
2025-03-02 19:55:20 +0000

    C pointers

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *puts_half - imprime la mitad
+ *@str: caractar dado
+ *
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int largo = 0;
+	int mitad = 0;
+
+	while (str[i])
+	{
+		i++;
+		largo++;
+	}
+	mitad = largo /2;
+
+	for (i = mitad; str[i]; i++)
+	{
+		_putchar(str[i]);
+	}
+_putchar(10);
+}
