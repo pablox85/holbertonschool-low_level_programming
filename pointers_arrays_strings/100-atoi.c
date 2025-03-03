e94c422c6097a83560f58713ee59dcb0647e7611
pablox85 <pdpcorrales@gmail.com>
2025-03-03 00:53:37 +0000

    C pointers

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,35 +1,33 @@
 #include "main.h"
+#include <limits.h>
 /**
- *
- *
- *
+ *_atoi - string a numero 
+ *@s: string dada
+ *Return: retorna el valor 
  */
 
 int _atoi(char *s)
 {
 
 	int i = 0;
-	int signo = 1;
+	int sign = 1;
 	int num = 0;
-	int digito = 0;
-
-		while (s[i])
-		{
-			if (s[i] == '-')
-			{
-				signo *= -1;
-			}
-			else if (s[i] >= '0' && s[i] <= '9')
-			{
-				num = num * 10 + (s[i] - '0');
-				digito = 1;
-			}
-			else if	(digito)
-			{
-				break;
-				i++;
-			}
-		}
-	return (num * signo);
-
+	int found_digit = 0;
+while (s[i])
+	{
+	if (s[i] == '-')
+		sign *= -1;
+	else if (s[i] >= '0' && s[i] <= '9')
+	{
+		if (num > (INT_MAX - (s[i] - '0')) / 10)
+		return (sign == 1 ? INT_MAX : INT_MIN);
+	num = num * 10 + (s[i] - '0');
+	found_digit = 1;
+	}
+	else if (found_digit)
+	break;
+	i++;
+	}
+return (num * sign);
 }
+
