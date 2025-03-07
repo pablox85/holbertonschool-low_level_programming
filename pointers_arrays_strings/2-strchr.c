35da42cc8c3c45b5c7e28a8fbacde26c95aff50e
pablox85 <102749675+pablox85@users.noreply.github.com>
2025-03-07 00:45:47 -0300

    Actualizar 2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -18,12 +18,12 @@ char *_strchr(char *s, char c)
 		{
 			return (&s[i]);
 		}
-		else if (s[i] == '\0')
-		{
-			return(&s[i]);
-		}
 		i++;
 
-	}
+			if (c == '\0')
+		{
+return (&s[i]);
+		}
+
 	return (NULL);
 }
