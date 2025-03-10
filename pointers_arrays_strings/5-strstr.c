59e330f2b3826c8d3135b19cdd922b5ae1429b71
pablox85 <pdpcorrales@gmail.com>
2025-03-10 01:25:05 +0000

    Works on strncpy

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ *_strstr - abajo del 1°while reiniciar
+ *@haystack: pajar
+ *@needle: aguuja
+ *Return: cadena encontrada
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	if (*needle == '\0')
+		{
+		return (haystack);
+		}
+
+			while (*haystack)
+			{
+				char *h = haystack;
+				char *n = needle;
+
+				while (*h && *n && (*h == *n))
+				{
+					h++;
+					n++;
+				}
+
+				if (*n == '\0')
+				{
+					return (haystack);
+				}
+				haystack++;
+
+			}
+
+	return (NULL);
+}
+
