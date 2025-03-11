2d9cd855fc4f642b296bb2a76ab476359a1809ad
pablox85 <pdpcorrales@gmail.com>
2025-03-11 07:12:31 +0000

    Malloc

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  *
  *
@@ -13,27 +14,13 @@ char *_strdup(char *str)
 	char *copy;
 	copy = strdup(str);
 
-	int i = 0;
 	
 	if (copy == NULL)
 	{
 		return (NULL);
 	}
 
-	while (copy[i] != '\0' && str[i] != '\0')
-	{
-		if (copy[i] == str[i])
-		{
-			i++;
-			
-		}
-		else
-		{
-			return NULL;
-		}
-	}
-	free(copy);
 
-
+return (0);
 }
 
