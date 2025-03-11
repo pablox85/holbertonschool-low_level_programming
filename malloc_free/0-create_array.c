4f6670c9f7add55a4c504853f3b695c77a7986ab
pablox85 <pdpcorrales@gmail.com>
2025-03-11 05:32:53 +0000

    Malloc

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ *
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int size;
+	char c;
+	char *arr;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	arr = malloc(sizeof(char) * 10);
+
+
+
+
+
+
+
+}
---
+++ malloc_free/main.h
@@ -0,0 +1,5 @@
+#include <unistd.h>
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+
