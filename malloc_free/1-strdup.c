837f3282a67e73eefe2a93dfe47eb47319b61d3c
pablox85 <pdpcorrales@gmail.com>
2025-03-12 06:20:23 +0000

    Malloc

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -2,25 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 /**
+ *_strdup - duplicadion de caractares
+ *@str: string dada
  *
- *
- *
- *
- *
+ *Return: valor de copy
  */
 
 char *_strdup(char *str)
 {
 	char *copy;
-	copy = strdup(str);
+	size_t largo = 0;
+	size_t strToStr = 0;
 
-	
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[largo] != '\0'
+	{
+		largo++;
+	}
+
+	copy = malloc(largo + 1);
+
 	if (copy == NULL)
 	{
 		return (NULL);
 	}
 
+	while (strToStr <= largo)
+	{
+		copy[strToStr] = str[strToStr];
+		strToStr++;
+	}
 
-return (0);
+return (copy);
+
 }
-
