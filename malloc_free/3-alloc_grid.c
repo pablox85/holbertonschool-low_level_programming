86034efc6e5e62bfe574f965025f239277aa55c1
pablox85 <pdpcorrales@gmail.com>
2025-03-14 21:19:15 +0000

    Malloc

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+int **alloc_grid(int width, int height)
+{
+	int i = 0, j = 0;
+
+	if (width == NULL)
+		return (NULL);
+	if(heigth == NULL)
+		return (NULL);
+
+
+
+
+
+
+
+
+
+
+};w
+
