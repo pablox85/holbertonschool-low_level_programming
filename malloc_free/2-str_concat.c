43f4f872cb9dca84525cae5c1c31071e56a934a5
pablox85 <pdpcorrales@gmail.com>
2025-03-14 20:24:08 +0000

    Malloc

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -4,52 +4,33 @@
 
 char *str_concat(char *s1, char *s2)
 {
-
 	size_t ss1 = 0, ss2 = 0, i = 0;
 	char *strN;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
 
+	if (s2 == NULL)
+		s2 = "";
 
 	while (s1[ss1] != '\0')
-	{
 		ss1++;
-	}
 
 	while (s2[ss2] != '\0')
-	{
 		ss2++;
-	}
-	
 
 	strN = malloc(ss1 + ss2 + 1);
 
 	if (strN == NULL)
-	{
-	
 		return (NULL);
-	}
 
 	for (i = 0; i < ss1; i++)
-	{
-	strN[i] = s1[i];
-	}
+		strN[i] = s1[i];
 
 	for (i = 0; i < ss2; i++)
-	{
-	strN[ss1 + i] = s2[i];
-	}
+		strN[ss1 + i] = s2[i];
 
 	strN[ss1 + ss2] = '\0';
 
 return (strN);
-
-
 }
