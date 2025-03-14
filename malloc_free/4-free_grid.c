48a8971408063815056baa9cccd0c6a93be24434
pablox85 <pdpcorrales@gmail.com>
2025-03-14 23:01:52 +0000

    Malloc

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,5 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
+/**
+ * free_grid - libera memoria
+ * @grid: puntero a filas
+ * @height: altura
+ * Return: nada
+ */
 
 void free_grid(int **grid, int height){
 
