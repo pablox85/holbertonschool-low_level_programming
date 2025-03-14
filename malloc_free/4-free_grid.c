faef67fd2c261cc46599e871a58987982e64f669
pablox85 <pdpcorrales@gmail.com>
2025-03-14 22:54:38 +0000

    Malloc

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -3,37 +3,13 @@
 
 int **alloc_grid(int width, int height)
 {
-	int w, h;
-	int **grid;
 
-	if (width <= 0)
-		return (NULL);
-	if(height <= 0)
-		return (NULL);
-
-	grid = malloc(sizeof(int *) * height);
-
-		if (grid == NULL)
-			return (NULL);
-
-	for(h = 0; h < height; h++)
+	int i;
+	
+	for (i = 0; i < height; i++)
 	{
-		grid[h] = malloc(sizeof(int) * width);
-		if (grid[h] == NULL)
-		{
-			for(; h >= 0; h--)
-				free(grid[h]);
-			
-			free(grid);
-		}
+		free(grid[i];
 	}
-
-	for (h = 0; h < height; h++)
-	{
-		for (w = 0; w < width; w++)
-			grid[h][w] = 0;
-	}
-	return (grid);
-
+free(grid);
 }
 
