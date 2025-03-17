eefa97a203cf3ccc05e603a3276cc10e1397d276
pablox85 <pdpcorrales@gmail.com>
2025-03-17 02:52:11 +0000

    more malloc

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+/**
+ * array_range - recorre el array en un range 
+ *@min: valor minimo
+ *@max: valor maximo
+ *Return: el valor dado
+ */
+
+int *array_range(int min, int max)
+{
+	size_t i = 0;
+	size_t arreglo = (max - min) +1;
+	int *arr = (int *)malloc(arreglo *  sizeof(int));
+	
+	if (min == 0)
+		return (NULL);
+	if (max == 0)
+		return (NULL);
+	if (min > max)
+		return(NULL);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < arreglo; i++)
+	{
+		arr[i] = min + 1;
+	}
+
+	return (arr);
+
+
+
+}
