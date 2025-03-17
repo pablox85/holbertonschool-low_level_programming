a210724c11d79adc2fd64c46de7445930b29b37d
pablox85 <pdpcorrales@gmail.com>
2025-03-17 02:28:06 +0000

    more malloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,41 +1,37 @@
-#include "main.h"
 #include <stdlib.h>
 #include <limits.h>
-#include <stdint.h>
-#include <string.h>
-/**
- *_calloc - copia de calloc
- *@nmemb: elementos a asignar
- *@size: tamaño de elementos
- *Return: memoria pedida
- */
 
+/**
+ * _calloc - una implementación de calloc.
+ * @nmemb: número de elementos a asignar.
+ * @size: tamaño de cada elemento.
+ * Return: puntero a la memoria asignada o NULL si falla.
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	size_t calcMemoria = nmemb * size;
-	size_t i;
-	void *memPedida = malloc(calcMemoria);
-	unsigned char *ptr = (unsigned char *)memPedida;
+    size_t calcMemoria;
+    void *memPedida;
+    unsigned char *ptr;
+    size_t i;
 
-	if (nmemb == 0)
-		return (NULL);
-	if (size == 0)
-		return (NULL);
+    if (nmemb == 0 || size == 0)
+        return (NULL);
+    if (nmemb > (SIZE_MAX / size))
+        return (NULL);
 
+    calcMemoria = nmemb * size;
 
-	if (nmemb > (SIZE_MAX / size))
-		return (NULL);
+    memPedida = malloc(calcMemoria);
 
+    if (memPedida == NULL)
+        return (NULL);
 
-	if (memPedida == NULL)
-		return (NULL);
+    ptr = (unsigned char *)memPedida;
+    for (i = 0; i < calcMemoria; i++)
+    {
+        ptr[i] = 0;  
+    }
 
-	for(i = 0; i < calcMemoria; i++)
-	{
-		ptr[i] = 0;
-	}
-		
-	return (memPedida);
-
-
+    return (memPedida);
 }
+
