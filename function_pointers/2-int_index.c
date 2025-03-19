a1df2cde94c5721f5d2d199bca26a25beaee48c6
pablox85 <pdpcorrales@gmail.com>
2025-03-19 20:57:18 +0000

    Buscar indice con funcion con comparacion en array

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+/**
+ *int_index - busca un int en un arrat
+ *@array: puntero para buscar en el array
+ *@size: numere de elemento en el []
+ *@cmp: puntero de funcion de comparacion
+ *Return: -1 si es menor a 0 || 0 si es mayor
+ *
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+
+
+}
