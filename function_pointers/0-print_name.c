8241154b48b0ed9271e0b91547f3b487b5db7ddc
pablox85 <pdpcorrales@gmail.com>
2025-03-19 19:03:35 +0000

    $mensaje

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.1h"
+#include <stdlib.h>
+/**
+ *print_name - imprime un arreglo dado por punteros
+ *@name: nombre, string a pasar
+ *@f: determina como se imprime name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+
+}
