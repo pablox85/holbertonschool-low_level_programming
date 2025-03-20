4e55ad487060c32e332eeee17d53be3f6b889818
pablox85 <pdpcorrales@gmail.com>
2025-03-20 16:41:00 +0000

    actulizacion betty

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -2,21 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+/**
+ *get_op_func - trae la cuenta a realizar
+ *@s: parametro dado
+ *Return: valor de i
+ */
 int (*get_op_func(char *s))(int, int)
 {
 
 op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	int i = 0;
 
-    while (ops[i].op != NULL)
+	while (ops[i].op != NULL)
 	{
 		if (strcmp(ops[i].op, s) == 0)
 		{
