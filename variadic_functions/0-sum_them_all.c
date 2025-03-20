788091e1700553e313e087df74a4bf6d3853ae56
pablox85 <pdpcorrales@gmail.com>
2025-03-20 18:42:57 +0000

    suma de parametros

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ *sum_them_all - suma todos los parametros
+ *@n: numero de argumentos a la funcion
+ *Return: suma de todos los parametros
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+
+	unsigned int i = 0;
+	int argSum = 0;
+	va_list args;
+
+	va_start(args, n);
+
+	if (n == 0)
+		return (0);
+
+	while (i < n)
+	{
+		argSum += va_arg(args, int);
+		i++;
+	}
+	return (argSum);
+}
