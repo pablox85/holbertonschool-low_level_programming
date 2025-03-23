bbca96275707348800426a6d17db14aae6d01cbc
root <pdpcorrales@gmail.com>
2025-03-23 23:58:39 +0000

    imprime numeros

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -3,30 +3,30 @@
 #include <stdio.h>
 
 /**
- * print_numbers - Imprime números separados por un separador.
+ * print_numbers - Imprime nums separados por un separador.
  * @separator: Separador entre los números.
  * @n: Cantidad de números.
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    unsigned int i;
-    va_list args;
+	unsigned int i;
+	va_list args;
 
-    va_start(args, n);
+	va_start(args, n);
 
-    for (i = 0; i < n; i++)
-    {
-        int num = va_arg(args, int);
-        printf("%d", num);
+	for (i = 0; i < n; i++)
+	{
+	int num = va_arg(args, int);
+	printf("%d", num);
 
-        /* Imprimir el separador solo si no es el último número */
-        if (separator != NULL && i < n - 1)
-        {
-            printf("%s", separator);
-        }
-    }
+	if (separator != NULL && i < n - 1)
+	{
+		printf("%s", separator);
+	}
 
-    printf("\n");
-    va_end(args);
+	}
+
+	printf("\n");
+	va_end(args);
 }
 
