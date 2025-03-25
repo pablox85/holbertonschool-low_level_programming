97acdf3bcab314685067c329994f95928b3eefd9
root <pdpcorrales@gmail.com>
2025-03-25 01:22:16 +0000

    update struct

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,24 @@
-#include "dog.h"
+#ifndef DOG_H
+#define DOG_H
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *struct dog- la estructura
+ *@name: nombre
+ *@age: edad
+ *@owner: dueÃo
+ */
+
+typedef struct dog
+{
+
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-dog_t *new_dog(char *name, float age, char *owner);
-
+void free_dog(dog_t *d);
+#endif
