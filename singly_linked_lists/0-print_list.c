1ce7c4ede8e86446b918553d85b37403145cd8a0
pablox85 <pdpcorrales@gmail.com>
2025-04-03 20:14:40 +0000

    file 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,22 +1,21 @@
 #include "lists.h"
 /**
- *print_list - lista 
+ *print_list - lista
  *@h: puntero con cosas....
  *Return: retorno
  */
 size_t print_list(const list_t *h)
 {
+	size_t count = 0;
 
- size_t count = 0;
-
-    while (h)
-    {
-        if (h->str)
-            printf("[%d] %s\n", h->len, h->str);
-        else
-            printf("[0] (nil)\n");
-        count++;
-        h = h->next;
-    }
-    return count;
+	while (h)
+	{
+		if (h->str)
+		printf("[%d] %s\n", h->len, h->str);
+	else
+		printf("[0] (nil)\n");
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
