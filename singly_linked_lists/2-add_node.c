58f7eb59191c6a899d010cf04caf7277e0e8a444
pablox85 <pdpcorrales@gmail.com>
2025-04-04 00:01:43 +0000

    half excs

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * add_node - agrega un nuevo nodo
+ * @head: inicio de la lista
+ * @str: no tengo idea
+ * Return: retorno de valores
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *nuevoNodo;
+	nuevoNodo = malloc(sizeof(*nuevoNodo)*);
+
+	if (malloc == NULL)
+		Return (NULL);
+
+	*nuevoNodo = str(strduo);
+		if (str == NULL);
+		{
+			free(*nuevoNodo);
+			Return (NULL);
+		}
+
+
+}
+
+
+
+
+
+}
