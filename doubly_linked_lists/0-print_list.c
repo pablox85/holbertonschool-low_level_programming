ed5b39488a396d9d533ff00e7b2a9b2c943a5cc8
pablox85 <pdpcorrales@gmail.com>
2025-04-07 02:35:16 +0000

    update lists.h

---
+++ doubly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dlistint - impirme la catidad de listas
+ * @h: listas que se le pasan
+ * Return: algo
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+
+
+
+
+
+}
+
