244635535633f910ad73fcfde3e93ee567c219ff
pablox85 <pdpcorrales@gmail.com>
2025-04-07 04:40:03 +0000

    nodo al final

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * add_dnodeint_end - agrega nodo al final
+ * @head: inicio nodos
+ * @n: nunca me acuerdo
+ * Return: los nodos(?)
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *nuevoNodo;
+	dlistint_t *temp;
+
+	temp = *head;
+
+
+	nuevoNodo = malloc(sizeof(dlistint_t));
+
+	if (nuevoNodo == NULL)
+		return (NULL);
+
+	nuevoNodo->n = n;
+	nuevoNodo->next = NULL;
+	nuevoNodo->prev = NULL;
+
+	if (*head == NULL)
+	{
+		*head = nuevoNodo;
+		return (nuevoNodo);
+	}
+
+	
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+
+		temp->next = nuevoNodo;
+		nuevoNodo->prev = temp;
+	
+
+	return (nuevoNodo);
+
+}
