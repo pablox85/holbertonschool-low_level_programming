91d5461d139f7ab619d0315857fd074fd91854fe
pablox85 <pdpcorrales@gmail.com>
2025-04-21 09:04:24 -0300

    update ex 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,45 +1,39 @@
 #include "main.h"
 #include <stdlib.h>
-#include <fnctl.h>
+#include <fcntl.h>
+#include <string.h>
 #include <unistd.h>
 /**
  * create_file - crear un archivo
  * @filename: nombre
  * @text_content: contenido
- * Return: solo dios sabe 
+ * Return: solo dios sabe
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, largo;
-	char *buffer, count;
-	
+	int len = 0;
+	int fd;
+	ssize_t w;
+
 	if (filename == NULL)
-		return (-1);
+	return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_TRUNC | O_WRONLY | O_CREAT, 0600);
+
 	if (fd == -1)
-		return (-1);
+	return (-1);
 
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
+		len = strlen(text_content);
+		w = write(fd, text_content, len);
+	}
+
+	if (w == -1 || w != len)
+	{
+		close(fd);
 		return (-1);
 	}
-	else if (text_content != NULL)
-	{
-		
-		if (buffer == NULL)
-		{
-			close(fd);
-			return (0);
-		}
-		while(text_content[largo] != '\0')
-		{
-			largo++;
-		}
-
-
-
-
-
-
+	close(fd);
+	return (1);
 }
--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
 #include "main.h"
