632fff16be2d1e67e2da49d85397858e17efda38
pablox85 <pdpcorrales@gmail.com>
2025-04-21 15:44:17 -0300

    update

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -37,8 +37,15 @@ if (fd_to == -1)
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
-bytes_read = read(fd_from, buffer, 1024);
 
+bytes_read = read(fd_from, buffer, 1024);
+if(byte_read == -1
+{
+    dprintf(STDERR_FILENO, "Error Can't read from file %s\n", file_from);
+    close(fd_from);
+    close(fd_to);
+    exit(98);
+})
 while (bytes_read > 0)
 {
 bytes_written = write(fd_to, buffer, bytes_read);
