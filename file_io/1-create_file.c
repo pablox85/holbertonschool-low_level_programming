0ba337d8d6293e6e8c313ca0ccb29036bd5709a3
pablox85 <pdpcorrales@gmail.com>
2025-04-21 09:11:06 -0300

    update exs

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -27,12 +27,12 @@ int create_file(const char *filename, char *text_content)
 	{
 		len = strlen(text_content);
 		w = write(fd, text_content, len);
-	}
 
-	if (w == -1 || w != len)
-	{
-		close(fd);
-		return (-1);
+		if (w == -1 || w != len)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 	close(fd);
 	return (1);
