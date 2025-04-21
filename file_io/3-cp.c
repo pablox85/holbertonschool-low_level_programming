85831123c1101c33c4c3747f7eda24fe73a8d4e5
pablox85 <pdpcorrales@gmail.com>
2025-04-21 17:03:47 -0300

    reset

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,72 +1,104 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-
-#define BUFFER_SIZE 1024
-
-/**
- * error_exit - Prints an error message and exits.
- * @code: Exit code.
- * @message: Format string with %s.
- * @value: String to insert.
- */
-void error_exit(int code, const char *message, const char *value)
-{
-	dprintf(STDERR_FILENO, message, value);
-	exit(code);
-}
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /**
- * error_exit_fd - Prints error for file descriptor and exits.
- * @code: Exit code.
- * @message: Format string with %d.
- * @fd: File descriptor value.
- */
-void error_exit_fd(int code, const char *message, int fd)
-{
-	dprintf(STDERR_FILENO, message, fd);
-	exit(code);
-}
-
-/**
- * main - Copies the content of one file to another.
+ * check_arguments - Validates number of arguments.
  * @argc: Argument count.
+ */
+void check_arguments(int argc)
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+}
+
+/**
+ * check_read - Checks if file descriptor is valid for reading.
+ * @fd: File descriptor.
+ * @filename: Name of the file.
+ */
+void check_read(int fd, char *filename)
+{
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+}
+
+/**
+ * check_write - Checks if file descriptor is valid for writing.
+ * @fd: File descriptor.
+ * @filename: Name of the file.
+ */
+void check_write(int fd, char *filename)
+{
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		exit(99);
+	}
+}
+
+/**
+ * check_close - Checks if a file descriptor was closed successfully.
+ * @fd: File descriptor.
+ */
+void check_close(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - Copies content from a file to another.
+ * @argc: Number of arguments.
  * @argv: Argument vector.
- * Return: 0 on success, exits with code on error.
+ * Return: 0 on success, exit with error code otherwise.
  */
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
 	ssize_t bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
+	char buffer[1024];
 
-	if (argc != 3)
-		error_exit(97, "Usage: cp file_from file_to\n", "");
-
+	check_arguments(argc);
 	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+	check_read(fd_from, argv[1]);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	check_write(fd_to, argv[2]);
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-		error_exit(99, "Error: Can't write to %s\n", argv[2]);
+	do {
+		bytes_read = read(fd_from, buffer, 1024);
+		if (bytes_read == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			check_close(fd_from);
+			check_close(fd_to);
+			exit(98);
+		}
+		if (bytes_read > 0)
+		{
+			bytes_written = write(fd_to, buffer, bytes_read);
+			if (bytes_written == -1)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+				check_close(fd_from);
+				check_close(fd_to);
+				exit(99);
+			}
+		}
+	} while (bytes_read > 0);
 
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1 || bytes_written != bytes_read)
-			error_exit(99, "Error: Can't write to %s\n", argv[2]);
-	}
-
-	if (bytes_read == -1)
-		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-
-	if (close(fd_from) == -1)
-		error_exit_fd(100, "Error: Can't close fd %d\n", fd_from);
-
-	if (close(fd_to) == -1)
-		error_exit_fd(100, "Error: Can't close fd %d\n", fd_to);
-
+	check_close(fd_from);
+	check_close(fd_to);
 	return (0);
 }
