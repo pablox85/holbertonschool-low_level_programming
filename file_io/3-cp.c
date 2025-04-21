1732bbe09f7af7d7d4c9ef68db1ad305dfdd8bfb
pablox85 <pdpcorrales@gmail.com>
2025-04-21 16:55:10 -0300

    update return

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -5,8 +5,8 @@
 #include <unistd.h>
 
 /**
- * check_arguments - Validates the number of arguments.
- * @argc: Number of command-line arguments.
+ * check_arguments - Validates number of arguments.
+ * @argc: Argument count.
  */
 void check_arguments(int argc)
 {
@@ -18,7 +18,7 @@ void check_arguments(int argc)
 }
 
 /**
- * check_read - Checks if file descriptor for reading is valid.
+ * check_read - Checks if file descriptor is valid for reading.
  * @fd: File descriptor.
  * @filename: Name of the file.
  */
@@ -32,7 +32,7 @@ void check_read(int fd, char *filename)
 }
 
 /**
- * check_write - Checks if file descriptor for writing is valid.
+ * check_write - Checks if file descriptor is valid for writing.
  * @fd: File descriptor.
  * @filename: Name of the file.
  */
@@ -46,8 +46,8 @@ void check_write(int fd, char *filename)
 }
 
 /**
- * check_close - Checks if a file descriptor closed properly.
- * @fd: File descriptor to close.
+ * check_close - Checks if a file descriptor was closed successfully.
+ * @fd: File descriptor.
  */
 void check_close(int fd)
 {
@@ -59,11 +59,10 @@ void check_close(int fd)
 }
 
 /**
- * main - Copies the content of a file to another file.
- * @argc: Number of command-line arguments.
- * @argv: Array of arguments.
- *
- * Return: 0 on success, exits with codes 97-100 on error.
+ * main - Copies content from a file to another.
+ * @argc: Number of arguments.
+ * @argv: Argument vector.
+ * Return: 0 on success, exit with error code otherwise.
  */
 int main(int argc, char *argv[])
 {
@@ -72,24 +71,12 @@ int main(int argc, char *argv[])
 	char buffer[1024];
 
 	check_arguments(argc);
-
 	fd_from = open(argv[1], O_RDONLY);
 	check_read(fd_from, argv[1]);
-
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	check_write(fd_to, argv[2]);
 
-	bytes_read = read(fd_from, buffer, 1024);
-	while (bytes_read > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			check_close(fd_from);
-			check_close(fd_to);
-			exit(99);
-		}
+	do {
 		bytes_read = read(fd_from, buffer, 1024);
 		if (bytes_read == -1)
 		{
@@ -98,7 +85,18 @@ int main(int argc, char *argv[])
 			check_close(fd_to);
 			exit(98);
 		}
-	}
+		if (bytes_read > 0)
+		{
+			bytes_written = write(fd_to, buffer, bytes_read);
+			if (bytes_written == -1)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+				check_close(fd_from);
+				check_close(fd_to);
+				exit(99);
+			}
+		}
+	} while (bytes_read > 0);
 
 	check_close(fd_from);
 	check_close(fd_to);
