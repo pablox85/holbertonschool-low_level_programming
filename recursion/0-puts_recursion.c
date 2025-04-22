f9ac37bf059a42556b7ed8aed1c277b99b3ba3df
pablox85 <pdpcorrales@gmail.com>
2025-04-22 12:48:01 -0300

    update recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts_recursion - imprime por recursion
+ * @s: ptr a s
+ */
+
+ void _puts_recursion(char *s)
+ {
+    
+    if (*s == '\0')
+    {
+        putchar('\n');
+        return;
+    }
+    if (s != '\0')
+    {
+        putchar(*s);
+        _puts_recursion (s + 1);
+    }
+    return;
+ }
