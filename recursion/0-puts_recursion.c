266bef146a437447d111111c787cb8be84b20d30
pablox85 <pdpcorrales@gmail.com>
2025-04-22 12:52:07 -0300

    update betty

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -4,18 +4,18 @@
  * @s: ptr a s
  */
 
- void _puts_recursion(char *s)
- {
-    
-    if (*s == '\0')
-    {
-        _putchar('\n');
-        return;
-    }
-    if (*s != '\0')
-    {
-        _putchar(*s);
-        _puts_recursion (s + 1);
-    }
-    return;
- }
+void _puts_recursion(char *s)
+{
+
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+if (*s != '\0')
+{
+_putchar(*s);
+_puts_recursion (s + 1);
+}
+return;
+}
